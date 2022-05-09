39c99125d8ee5a34d0b842e862cce2c9548d58e0
Luísa Vitória <luisa.vitoria@hotmail.com>
2022-05-09 15:14:56 -0300

    desafio finalizado

--- Desafio VAGAS.com/index.js
+++ Desafio VAGAS.com/index.js
@@ -9,8 +9,10 @@ let valueCounter = document.getElementById('counter--number').innerText
 const form = document.getElementById('form')
 const checkboxes = document.querySelectorAll('input[name="sticker"]')
 const arrayCheckboxes = [...checkboxes]
+const labelCheckboxes = document.getElementsByClassName('checkbox--label')
 const btnSubmit = document.getElementById('button--submit')
 btnSubmit.disabled = true
+const divSuccessMessage = document.getElementById('success--message')
 
 
 btnDecrease.onclick = function(e) {
@@ -35,6 +37,7 @@ btnIncrease.onclick = function(e) {
     btnDecrease.disabled = false
     btnDecrease.style.backgroundColor = '#2F3676'  
     divCounter.innerText = valueCounter
+    divCounter.style.backgroundColor = '#DDE3E9'
 }
 
 function checkboxesChecked() {
@@ -51,24 +54,58 @@ function checkboxesChecked() {
         btnSubmit.style.backgroundColor = '#2F3676'
         btnSubmit.style.color = '#fff'
         btnSubmit.style.border = 'none'
+        divSuccessMessage.innerText = ''
+
+        Array.from(labelCheckboxes).forEach(item => {
+            item.style.setProperty('--labelBeforeBackColor', '#2F3676')
+        })
     }
+
     return numberChecked
-   
-    
 }
 
 arrayCheckboxes.forEach(element => {
     element.onclick = checkboxesChecked
 })
 
+btnSubmit.onclick = function(e) {
+    e.preventDefault()
 
-
-
-btnSubmit.onclick = function() {
     const numberChecked = checkboxesChecked(arrayCheckboxes)
     console.log(numberChecked)
 
-    if(numberChecked <= 0) {
-        alert('teste')
+    if(valueCounter <= 0 && numberChecked <= 0) {
+        divCounter.style.backgroundColor = '#F59393'
+        btnDecrease.style.backgroundColor = '#2F3676'
+
+        return
+    }else if(valueCounter <= 0) {
+        divCounter.style.backgroundColor = '#F59393'
+        btnDecrease.style.backgroundColor = '#2F3676'
+
+        return
+    } else if(numberChecked <= 0) {
+        Array.from(labelCheckboxes).forEach(item => {
+            item.style.setProperty('--labelBeforeBackColor', '#F59393')
+        })
+        
+        return
+    }else {
+        divSuccessMessage.innerText = 'Formulário enviado com sucesso!'
+
+        setTimeout(() => {
+            form.submit()
+        }, 5000);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
